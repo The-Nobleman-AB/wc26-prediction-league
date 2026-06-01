06143942cd0f96494b1e006d7f304d188c96f0e6
The-Nobleman-AB <150331365+The-Nobleman-AB@users.noreply.github.com>
2026-06-01 16:07:37 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 const API_URL =
-"https://script.google.com/macros/s/AKfycbweBLtA3_DTqErEqOz0TDopxBZVByVtkQ-2DIN1PiCMFJVdG9pKIDlIdZ3zqurDr1Ihcw/exec";
+"https://script.google.com/macros/s/AKfycbzYMZs6gOZ0iINkoCN20YVaB0JXB7UvQ5kYHZf5B-X6X-lpP_9-RzgAXvtXhQt14fM0gw/exec";
